2370f39de10782902af8def04bf28dc3f1349cfc
Borna Butkovic <bornabutkovic@gmail.com>
2019-04-23 23:51:57 +0200

    Minor cleanup, add getTranslation helper

--- app/Traits/Translatable.php
+++ app/Traits/Translatable.php
@@ -8,19 +8,28 @@ use App\Language;
 trait Translatable {
     use DTranslatable {
         DTranslatable::translate as parentTranslate;
+        DTranslatable::getTranslation as parentGetTranslation;
     }
 
     /**
      * Since we are storing the list of locales in a database table, we are going to
      * pass a Language model to our translate function instead of a code
      *
-     * @param Language $language
      * @param bool        $withFallback
      *
      * @return \Illuminate\Database\Eloquent\Model|null
      */
-    public function translate(Language $language, $withFallback = false) {
-        $locale = $language->id;
-        return $this->parentTranslate($locale, $withFallback);   
+    public function translate($language, $withFallback = null) {
+        if($language instanceof Language) {
+            $language = $language->id;
+        }
+        return $this->parentTranslate($language, $withFallback);   
+    }
+
+    public function getTranslation($language, $withFallback = null) {
+        if($language instanceof Language) {
+            $language = $language->id;
+        }
+        return $this->parentGetTranslation($language, $withFallback);
     }
 }
