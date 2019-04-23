d8aff0840d1fed14550a1baf9f621d402dd0b60b
Borna Butkovic <bornabutkovic@gmail.com>
2019-04-23 23:47:47 +0200

    Add translation to categories

--- app/Category.php
+++ app/Category.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use \App\Traits\Translatable;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use Translatable;
+
+    public $translatedAttributes = ['title'];
+
 }
