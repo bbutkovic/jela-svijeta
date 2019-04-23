85452a925b70914ade921f5c0dcb96fe111989fa
Borna Butkovic <bornabutkovic@gmail.com>
2019-04-23 11:52:14 +0200

    Add basic meal filtering + listing of meals

--- app/Http/Controllers/Api/MealController.php
+++ app/Http/Controllers/Api/MealController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Meal;
 use App\Language;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Requests\MealListingRequest;
 use App\Http\Controllers\Controller;
@@ -11,13 +12,46 @@ use App\Http\Controllers\Controller;
 class MealController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display the list of meals filtered by entered query parameters.
      *
      * @return \Illuminate\Http\Response
      */
     public function index(MealListingRequest $request, Language $lang)
     {
-        //
+        $mq = (new Meal)->newQuery();
+
+        //Select which data to include in the response
+        if($request->has('with')) {
+            $mq->with($request->input('with'));
+        }
+
+        //Filter search by category ID
+        if($request->has('category')) {
+            $mq->byCategory($request->input('category'));
+        }
+
+        //Filter search by tags, every record must have every requested tag
+        if($request->has('tags')) {
+            $mq->byTags($request->input('tags'));
+        }
+
+        //Filter search by touched after timestamp
+        $diffTime = null;
+        if($request->has('diff_time')) {
+            $diffTime = Carbon::parse($request->input('diff_time'));
+            $mq->touchedAfter($diffTime);
+        }
+
+        $meals = $mq->get();
+        
+        //In case we filtered the request by diff_time also check status
+        if($diffTime) {
+            $meals->each(function($meal) use($diffTime) {
+                $meal->setStatusAfter($diffTime);
+            });
+        }
+
+        return $meals;
     }
 
     /**
